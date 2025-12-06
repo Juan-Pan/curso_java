0695ca5f52eab932e34457ef504ce8b5130ff9db
Juan David López Tapasco <j.lopez18@utp.edu.co>
2025-12-06 20:27:47 +0100

    update

--- ClasesYObjetos/src/persona/Persona.java
+++ ClasesYObjetos/src/persona/Persona.java
@@ -6,29 +6,34 @@ public class Persona {
 
     // constructor
 
-    public Persona(String nombre, String apellido)
-    {
+    public Persona(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
     }
 
-    //metodos
+    // metodos
     public String getnombre() // lee informacion y la retorna
     {
         return this.nombre;
     }
 
-    public void setNombre(String nombre) //modifica informacion
+    public void setNombre(String nombre) // modifica informacion
     {
         this.nombre = nombre;
     }
+
     public String getApellido() // lee informacion y la retorna
     {
         return this.apellido;
     }
-    public void setApellido(String apellido) //modifica informacion
+
+    public void setApellido(String apellido) // modifica informacion
     {
         this.apellido = apellido;
     }
-}
 
+    // metodos adicionales
+    public void mostrarPersona() {
+        System.out.println("Nombre: " + this.nombre + " Apellido: " + this.apellido);
+    }
+}
--- ClasesYObjetos/src/persona/PruebaPersona.java
+++ ClasesYObjetos/src/persona/PruebaPersona.java
@@ -5,7 +5,7 @@ public class PruebaPersona {
         System.out.println("***Creacion de objetos de la clase persona***");
         //intanciamos un nuevo objeto
         Persona persona1 = new Persona("Juan","Pedrin");
-        System.out.println("Persona 1: " + persona1.getnombre() +" "+ persona1.getApellido());
+        persona1.mostrarPersona();
     }
     
 }
